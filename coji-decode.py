9bc78d27b9a96055e798392e278d4162172b31d2
Max <maxfyk@gmail.com>
2022-08-19 20:05:45 +0800

    update api tests

--- coji-decode.py
+++ coji-decode.py
@@ -1,16 +1,20 @@
 import io
+import time
 import base64
 
 import requests as r
 
 in_data = {
-  'type': 'photo',
-  'photo': None,
-  'user-id': 'asd22',
+    'decode-type': 'image',
+    'in-data': None,
+    'user-id': 'asd22',
+    'style-info': {
+        'name': 'geom-original',
+    }
 }
 
-
-in_data['photo'] = base64.encodebytes(open('out.jpg', 'rb').read()).decode()
-
-resp = r.post('http://localhost:8000/coji-code/decode', json=in_data)
+in_data['in-data'] = base64.encodebytes(open('photo_2022-08-12_16-24-41.jpg', 'rb').read()).decode()
+start_time = time.time()
+resp = r.get('http://localhost:8000/coji-code/decode', json=in_data)
+print("--- %s seconds ---" % (time.time() - start_time))
 print(resp.json())
